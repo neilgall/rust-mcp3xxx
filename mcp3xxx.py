06d6904eb9a0c1225a1bb8140e73dceba0e42a5e
Neil Gall <neilgall@mac.com>
2019-04-16 20:54:22 +0100

    Extend to three channels

--- mcp3xxx.py
+++ mcp3xxx.py
@@ -6,7 +6,7 @@ from adafruit_mcp3xxx.mcp3008 import MCP3008
 from adafruit_mcp3xxx.analog_in import AnalogIn
 
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
-cs = digitalio.DigitalInOut(board.D23)
+cs = digitalio.DigitalInOut(board.D22)
 mcp = MCP3008(spi, cs)
 chan = AnalogIn(mcp, 0)
 
--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,23 @@ use std::time::Duration;
 use mcp3xxx::*;
 
 fn main() {
-	let mut mcp = MCPDevice::new(rustpi_io::serial::Device::CE0, MCPDeviceType::MCP3008, 23)
+	let mut mcp = MCPDevice::new(rustpi_io::serial::Device::CE0, MCPDeviceType::MCP3008, 22)
 		.expect("can't open device");
 
 	let ch0 = mcp.single_analog_in(0)
-		.expect("can't get analog in channel");
+		.expect("can't get analog in channel 0");
+
+	let ch1 = mcp.single_analog_in(1)
+		.expect("can't get analog in channel 1");
+
+	let ch2 = mcp.single_analog_in(2)
+		.expect("can't get analog in channel 2");
 
 	loop {
-		println!("{}", mcp.read_value(&ch0).expect("can't read value"));
+		let r0 = mcp.read_value(&ch0).expect("can't read value from channel 0");
+		let r1 = mcp.read_value(&ch1).expect("can't read value from channel 1");
+		let r2 = mcp.read_value(&ch2).expect("can't read value from channel 2");
+		println!("{} {} {}", r0, r1, r2);
 		thread::sleep(Duration::from_millis(500));
 	}
 }
